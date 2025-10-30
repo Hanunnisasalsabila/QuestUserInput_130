ec4cb912aa81f5002dc3eae14e77a6085fdd9ee8
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-10-30 16:26:49 +0700

    Add Submit Button

--- app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit.kt
@@ -87,6 +87,17 @@ fun FormDataDiri(modifier: Modifier = Modifier
             thickness = dimensionResource(R.dimen.divider_tipis),
             color = Color.DarkGray
         )
+        Button(
+            modifier = Modifier.fillMaxWidth(1f),
+            enabled = textAlamat.isNotEmpty(),
+            onClick = {
+                nama=textNama
+                jenis=textJK
+                alamat=textAlamat
+            }
+        ){
+            Text(stringResource( R.string.submit))
+        }
     }
 
 }
