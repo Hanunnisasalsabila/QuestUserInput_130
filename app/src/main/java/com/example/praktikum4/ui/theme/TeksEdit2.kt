471aa1dfebbe0122cbaeedca1c9b3c316f2913e7
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-01 11:01:34 +0700

    Membuat struktur dasar tampilan dengan Column dan Box

--- app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit2.kt
+++ app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit2.kt
@@ -1,11 +1,17 @@
 package com.example.praktikum4.ui.theme
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import com.example.praktikum4.R
 
 @Composable
 fun FormFormulirPendaftaran() {
@@ -27,5 +33,16 @@ fun FormFormulirPendaftaran() {
     val purpleBackground = Color(0xFFE6D5F5)
     val purpleButton = Color(0xFF7C3AED)
     val purpleHeader = Color(0xFFB794F6)
+
+    Column(
+        modifier = modifier
+            .background(purpleBackground)
+            .padding(dimensionResource(R.dimen.padding_small))
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
 
 }
