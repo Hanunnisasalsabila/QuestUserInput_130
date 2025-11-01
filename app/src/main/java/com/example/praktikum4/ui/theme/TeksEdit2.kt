0ba565dbd33bd345cfffaba5395d27074eb56a34
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-01 11:38:06 +0700

    menambah Isi Form untuk bagian nama lengkap

--- app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit2.kt
+++ app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit2.kt
@@ -4,9 +4,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,6 +17,8 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -130,7 +134,17 @@ fun FormFormulirPendaftaran() {
                         .weight(1f)
                         .verticalScroll(rememberScrollState()),
                 ) {
-
+                    OutlinedTextField(
+                        value = textNama,
+                        singleLine = true,
+                        shape = MaterialTheme.shapes.large,
+                        modifier = Modifier.fillMaxWidth(),
+                        label = { Text(text = "Nama Lengkap") },
+                        onValueChange = {
+                            textNama = it
+                        }
+                    )
+                    Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_small)))
                 }
             }
         }
