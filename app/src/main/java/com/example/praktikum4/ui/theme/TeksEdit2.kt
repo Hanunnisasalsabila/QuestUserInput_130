174236cb76e99723f270ccf3a89499d040821d30
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-01 10:51:47 +0700

    Menambahkan state untuk input teks (nama, alamat, jenis kelamin, status)

--- app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit2.kt
+++ app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit2.kt
@@ -1,8 +1,17 @@
 package com.example.praktikum4.ui.theme
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
 
 @Composable
 fun FormFormulirPendaftaran() {
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textStatus by remember { mutableStateOf("") }
 
 }
