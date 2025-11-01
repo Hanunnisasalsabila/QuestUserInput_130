e95f20a8ed5445c5d28a601b54ee8d5f9ae4f62f
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-01 10:52:37 +0700

    Menambahkan state tambahan untuk menyimpan data hasil submit

--- app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit2.kt
+++ app/src/main/java/com/example/praktikum4/ui/theme/TeksEdit2.kt
@@ -14,4 +14,9 @@ fun FormFormulirPendaftaran() {
     var textJK by remember { mutableStateOf("") }
     var textStatus by remember { mutableStateOf("") }
 
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
+
 }
